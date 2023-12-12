951f3a1f8d58193cf473c49900a767f17efe6221
laska-adief <laskaadief.la@gmail.com>
2023-12-12 10:51:47 +0700

    [React Query Learning] display data heroes

--- src/components/Superheroes.tsx
+++ src/components/Superheroes.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { Hero } from "../types";
 import { fetchAllHeroes } from "../hooks/heores/fetchHeroes";
 
 const Superheroes = () => {
@@ -11,6 +12,17 @@ const Superheroes = () => {
     return <h2>Loading...</h2>;
   }
 
-  return <div>Superheroes</div>;
+  return (
+    <div>
+      <h2>Superheroes</h2>
+      <ul className="hero-list">
+        {data?.map((item: Hero) => (
+          <li key={item?.id}>
+            {item?.name} | {item?.actor}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default Superheroes;
