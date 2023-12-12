5b8e68e106dff233eee8fb3956bc385143b94e34
laska-adief <laskaadief.la@gmail.com>
2023-12-12 10:48:49 +0700

    [React Query Learning] add QueryClientProvider in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Superheroes from "./components/Superheroes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Navbar />
       <div className="container">
         <Routes>
@@ -18,7 +20,7 @@ function App() {
           </Route>
         </Routes>
       </div>
-    </>
+    </QueryClientProvider>
   );
 }
 
