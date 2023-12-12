22d0c981f98ccc4f427850330b0db1c70eada1ba
laska-adief <laskaadief.la@gmail.com>
2023-12-12 10:50:56 +0700

    [React Query Learning] fecth data heroes using useQuery

--- src/components/Superheroes.tsx
+++ src/components/Superheroes.tsx
@@ -1,4 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchAllHeroes } from "../hooks/heores/fetchHeroes";
+
 const Superheroes = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ["superheroes"],
+    queryFn: fetchAllHeroes,
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
   return <div>Superheroes</div>;
 };
 export default Superheroes;
